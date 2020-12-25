113873aa367d03380e606023617bd86ee9588789
Jaclyn Chen <watertranquil@gmail.com>
2020-12-25 13:30:33 +0800

    Initial Commit

---
+++ LargeTitlesIssues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LargeTitlesIssues
+//
+//  Created by Jaclyn Chen on 12/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
