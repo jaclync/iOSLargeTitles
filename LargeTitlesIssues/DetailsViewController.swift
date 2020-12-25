788156b35d938b7cb393dbac880ed39a59061612
Jaclyn Chen <watertranquil@gmail.com>
2020-12-25 14:03:53 +0800

    Add `DetailsViewController` and configure main window to show a tab bar controller with a navigation controller of `ViewController`.

---
+++ LargeTitlesIssues/DetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.swift
+//  LargeTitlesIssues
+//
+//  Created by Jaclyn Chen on 12/25/20.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "Details view"
+
+        view.backgroundColor = .orange
+    }
+}
--- LargeTitlesIssues/SceneDelegate.swift
+++ LargeTitlesIssues/SceneDelegate.swift
@@ -16,7 +16,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+
+        let tabBarController = UITabBarController()
+        configureTabBarController(tabBarController)
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,3 +61,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+private extension SceneDelegate {
+    func configureTabBarController(_ tabBarController: UITabBarController) {
+        tabBarController.view.backgroundColor = .orange
+
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+
+        tabBarController.viewControllers = [
+            navigationController
+        ]
+    }
+}
